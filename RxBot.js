18e42ab9dfc77ba431521a5605f36ceb927f411c
joe-Ru <ruizjoseph17@gmail.com>
2020-03-24 20:00:56 -0400

    Removed required statements
    Apparently there were some conflicts regarding these statements
    const axios = require('axios');
    const fetch = require('node-fetch');
    const Discord = require('discord.js');
    const { Client, RichEmbed } = require('discord.js');
    
    As each of them needed the require() function to be defined.

--- RxBot.js
+++ RxBot.js
@@ -1,26 +1,18 @@
-const Discord = require('discord.js')
-const axios = require('axios')
-const fetch = require('node-fetch');
-const { Client, RichEmbed } = require('discord.js');
-const client = new Client();
+import DISCORD_TOKEN from "./token.js";
+import Discord from "discord.js";
+
+
+const client = new Discord.Client();
+
 const prefix = '!';
-
-const medAnnouncement = "The RxBot is not to be taken as medical advice, if you are in trouble\
+const MED_ANNOUNCEMENT = "The RxBot is not to be taken as medical advice, if you are in trouble\
 please contact your physician or call 911 if this is a medical emergency. ";
-
-//Sends picture of a cat
-const sendCats = async (message, numCats) => {
-    const catImages = (await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${numCats}`)).data;
-    for (const catImage of catImages) {
-      message.channel.send(new Discord.Attachment(catImage.url));
-    }
-  }
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 })
 
-
+//Urban dictionary command
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -47,6 +39,16 @@ client.on('message', async message => {
 });
 
 
+// #### CAT FACT/PICTURE FUNCTIONALITY ####
+//Sends picture of a cat
+const sendCats = async (message, numCats) => {
+  const catImages = (await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${numCats}`)).data;
+  for (const catImage of catImages) {
+    message.channel.send(new Discord.Attachment(catImage.url));
+  }
+}
+
+// Calls on the sendCats 'function
 client.on('message', message => {
     if (message.content === '!cat') {
       sendCats(message, 1);
@@ -70,17 +72,35 @@ client.on('message', async message => {
 
 
 client.on('message', async message => {
-  var medicine;
-  if(message.content == '!medFact ' + medicine){
-    message.channel.send(medAnnouncement);
+  
+  if(message.content === '!medFact'){
 
     //TODO: Look into parsing the JSON output from the API to a variable and then accessing the info from there.
-    var parsing = JSON.parse()
-    const {reply} = await fetch('https://api.fda.gov/drug/event.json?search=' + medicine + )
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    const querystring = require('querystring');
+  
+    // Start of Urban command
+    if (command === 'medFact') {
+      if (!args.length) {
+        return message.channel.send('You need to supply a search term!');
+      }
+  
+    const query = querystring.stringify({ term: args.join(' ') });
+  
+    const { list } = await fetch(`https://api.fda.gov/drug/event.json?search=${query}`).then(response => response.json());
+    
+    if (!list.length) {
+      return message.channel.send(`No results found for **${args.join(' ')}**.`);
+    }
+    
+    message.channel.send(MED_ANNOUNCEMENT);
+    message.channel.send(list[0].definition);
   }
+}
 })
 
-  /*
+  /* Look into this.
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) {
         return
@@ -88,15 +108,7 @@ client.on('message', (receivedMessage) => {
 
     receivedMessage.channel.send("Message received: " + receivedMessagae.author.toString() + receivedMessage.content)
 })
-*/
-
-
-
-/* 
-
 Use this for drug API https://api.fda.gov/drug/event.json?limit=1
-
-where 
-
 */ 
-client.login("token goes here");
+
+client.login(DISCORD_TOKEN);
